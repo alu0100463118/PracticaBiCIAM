ff9ad602271b385588f04879e9de1667e62f2795
alu0100463118 <alu0100463118@ull.edu.es>
2016-01-18 04:56:04 +0000

    Pruebas creadas

--- src/local_search/complement/StopExecute.java
+++ src/local_search/complement/StopExecute.java
@@ -3,7 +3,7 @@ package local_search.complement;
 
 public class StopExecute {
 		
-	public Boolean stopIterations(int countIterationsCurrent, int countmaxIterations) {
+	public static Boolean stopIterations(int countIterationsCurrent, int countmaxIterations) {
 		if (countIterationsCurrent < countmaxIterations) {
 			return false;
 		} else {
--- test/pruebas.java
+++ test/pruebas.java
@@ -1,12 +1,25 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+//import local_search.candidate_type.*;
+import local_search.complement.StopExecute;
 
 public class pruebas {
 
 	@Test
 	public void test1() {
-		fail("Not yet implemented");
+		int numeroActualIteraciones = 4;
+		int numeroMaximoIteraciones = 4;
+		boolean salir = StopExecute.stopIterations(numeroActualIteraciones, numeroMaximoIteraciones);
+		assertTrue(salir);
 	}
 
+	@Test
+	public void test2() {
+		int numeroActualIteraciones = 2;
+		int numeroMaximoIteraciones = 4;
+		boolean salir = StopExecute.stopIterations(numeroActualIteraciones, numeroMaximoIteraciones);
+		assertFalse(salir);
+	}
+	
 }
